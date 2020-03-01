908a65d2d2d87d45cf0d4333573135195705c1b9
Kelsey Moty <7917220+kelseymoty@users.noreply.github.com>
2020-03-01 00:48:41 -0500

    added specific condition and updated config

--- src/STUDY-CONFIG.js
+++ src/STUDY-CONFIG.js
@@ -52,6 +52,373 @@ var startGame_girl_gen = [
   },
 ]
 
+var warmup = [
+  {
+    type: 'action',
+    id: 'warmup1',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/warmup/warmup1.mp4',
+          coordinates: [
+            { value: 'sandwich', shape: "rect", coordinates: "80, 450, 400, 715" },
+            { value: 'banana', shape: "rect", coordinates: "600, 450, 970, 715" },
+            { value: 'apple', shape: "rect", coordinates: "1300, 450, 1600, 715" },
+          ]
+        },
+      }
+    ]
+  },
+  {
+    type: 'action',
+    id: 'warmup2',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/warmup/warmup2.mp4',
+          coordinates: [
+            { value: 'shoes', shape: "rect", coordinates: "60, 350, 400, 715" },
+            { value: 'backpack', shape: "rect", coordinates: "650, 350, 950, 715" },
+            { value: 'socks', shape: "rect", coordinates: "1300, 350, 1550, 715" },
+          ]
+        },
+      }
+    ]
+  },
+  {
+    type: 'action',
+    id: 'warmup3',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/warmup/warmup3.mp4',
+          coordinates: [
+            { value: 'pants', shape: "rect", coordinates: "100, 250, 360, 775" },
+            { value: 'jacket', shape: "rect", coordinates: "630, 250, 980, 775" },
+            { value: 'hat', shape: "rect", coordinates: "1250, 350, 1550, 715" },
+          ]
+        },
+      }
+    ]
+  },
+  {
+    type: 'action',
+    id: 'pragmatics_intro',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/pragmatics_intro.mp4',
+        },
+      }
+    ]
+  }
+]
+
+
+///////// Pragmatic Tasks /////////
+
+var yarp = [
+  [
+    {
+      type: 'action',
+      id: 'yarp_girl',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/yarping_girl.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'action',
+      id: 'yarp_boy',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/yarping_boy.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'control',
+      shuffle: true
+    }
+  ]
+
+]
+
+var nilt = [
+  [
+    {
+      type: 'action',
+      id: 'nilt_girl',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/nilting_girl.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'action',
+      id: 'nilt_boy',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/nilting_boy.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'control',
+      shuffle: true
+    }
+  ]
+
+]
+
+var gaz = [
+  [
+    {
+      type: 'action',
+      id: 'gaz_girl',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/gazzing_girl.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'action',
+      id: 'gaz_boy',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/gazzing_boy.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'control',
+      shuffle: true
+    }
+  ]
+
+]
+
+var leem = [
+  [
+    {
+      type: 'action',
+      id: 'leem_girl',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/leeming_girl.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'action',
+      id: 'leem_boy',
+      stimuli: [
+        {
+          type: 'movie',
+          parameters: {
+            filename: '../../../assets/movie/pragmatics/leeming_boy.mp4',
+            coordinates: [
+              { value: 'yes', shape: "rect", coordinates: "300, 400, 525, 700" },
+              { value: 'no', shape: "rect", coordinates: "1110, 400, 1350, 700" },
+            ]
+          },
+        }
+      ]
+    },
+    {
+      type: 'control',
+      shuffle: true
+    }
+  ]
+
+]
+
+///// Girl Generic /////
+var yarp_girl_gen = [
+  {
+    type: 'action',
+    id: 'yarp_girl_gen',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/yarping_girl_gen.mp4',
+        },
+      }
+    ]
+  },
+  yarp
+]
+
+var nilt_girl_gen = [
+  {
+    type: 'action',
+    id: 'nilt_girl_gen',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/nilting_girl_gen.mp4',
+        },
+      }
+    ]
+  },
+  nilt
+]
+
+var gaz_girl_gen = [
+  {
+    type: 'action',
+    id: 'gaz_girl_gen',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/gazzing_girl_gen.mp4',
+        },
+      }
+    ]
+  },
+  gaz
+]
+
+var leem_girl_gen = [
+  {
+    type: 'action',
+    id: 'leem_girl_gen',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/leeming_girl_gen.mp4',
+        },
+      }
+    ]
+  },
+  leem
+]
+
+///// Girl Specific /////
+var yarp_girl_spec = [
+  {
+    type: 'action',
+    id: 'yarp_girl_spec',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/yarping_girl_spec.mp4',
+        },
+      }
+    ]
+  },
+  yarp
+]
+
+var nilt_girl_spec = [
+  {
+    type: 'action',
+    id: 'nilt_girl_spec',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/nilting_girl_spec.mp4',
+        },
+      }
+    ]
+  },
+  nilt
+]
+
+var gaz_girl_spec = [
+  {
+    type: 'action',
+    id: 'gaz_girl_spec',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/gazzing_girl_spec.mp4',
+        },
+      }
+    ]
+  },
+  gaz
+]
+
+var leem_girl_spec = [
+  {
+    type: 'action',
+    id: 'leem_girl_spec',
+    stimuli: [
+      {
+        type: 'movie',
+        parameters: {
+          filename: '../../../assets/movie/pragmatics/leeming_girl_spec.mp4',
+        },
+      }
+    ]
+  },
+  leem
+]
+
 ///////// Essentialism Tasks /////////
 
 ///// Causal Explanations /////
@@ -394,7 +761,6 @@ var homogeneity_girl = [
   ]
 ]
 
-
 ///// Naturalness /////
 var natural_girl = [
   {
@@ -470,6 +836,17 @@ var natural_girl = [
 var girl_gen = [
   forms,
   startGame_girl_gen,
+  warmup,
+  [
+    yarp_girl_gen,
+    leem_girl_gen,
+    nilt_girl_gen,
+    gaz_girl_gen,
+    {
+      type: 'control',
+      shuffle: true
+    },
+  ],
   causal_girl,
   [
     stability_girl,
@@ -486,6 +863,17 @@ var girl_gen = [
 var girl_spec = [
   forms,
   startGame_girl_spec,
+  warmup,
+  [
+    yarp_girl_spec,
+    leem_girl_spec,
+    nilt_girl_spec,
+    gaz_girl_spec,
+    {
+      type: 'control',
+      shuffle: true
+    },
+  ],
   causal_girl,
   [
     stability_girl,
@@ -507,7 +895,7 @@ export const Project = {
     girl_spec,
     girl_gen
   ],
-  name: "gender_pragmactics",
+  name: "gender_pragmatics",
   ended: {
     type: 'action',
     id: 'form',
